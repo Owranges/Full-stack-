af8bac40e05cfce8b76abd049f013b8a13669349
Utilisateur <frank.owen.brunet@gmail.com>
2020-07-24 09:52:25 +0200

    step1 terminer

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-let heloo = []
+const MongoClient = require("mongodb").MongoClient;       /* Module MongoDb */
+const url = 'mongodb://localhost:27017/'                  /* Url MongoDB */
+const dbName = 'Groupe_data_base'                         /* Data base name*/
+let db;
+let client;
+
+async function database(){                    
+    try{
+        client = await MongoClient.connect(url, { useNewUrlParser: true , useUnifiedTopology: true });
+        db = client.db(dbName);
+        await db.createCollection("Students");
+        await db.createCollection("Groups");    
+
+    }catch(e){
+        console.log(e);
+
+    }finally{
+        client.close();
+    }
+}
+
+database();
